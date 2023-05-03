ff1f7aa1b31d90865cb2f70f73ff2c39b879f909
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-05-03 10:19:16 -0400

    Finished drivetrain file

--- robotcontainer.py
+++ robotcontainer.py
@@ -1,1 +1,17 @@
 import wpilib as wp
+from climbup import climbup
+from drivetrain import Drivetrain as DT
+
+class RobotContainer:
+    def __init__(self):
+        self.controller = wp.Joystick(0)
+        self.drivetrain = DT()
+        self.chooser = wp.SendableChooser()
+        self._configure()
+
+    def _configure(self):
+        self.chooser.setDefaultOption("Climb Up", climbup(self.drivetrain))
+        wp.SmartDashboard.putData(self.chooser)
+
+    def get_autonomous(self):
+        return self.chooser.getSelected()
